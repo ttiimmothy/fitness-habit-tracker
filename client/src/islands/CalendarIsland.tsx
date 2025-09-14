ceb7b2a48dee5ab1ddaecad1e80d3f5a97382147
Timothy <dinosauli2006@gmail.com>
2025-09-14 07:26:33 -0400

    fix imports error

--- client/src/islands/CalendarIsland.tsx
+++ client/src/islands/CalendarIsland.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Calendar} from './react/Calendar';
-import { useCalendarLogs } from '../hooks/useCalendarLogs';
+import { useCalendarLogs } from '../hooks/useStats';
 import {QueryProvider} from "../providers/QueryProvider"
 
 interface CalendarIslandProps {
